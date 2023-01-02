d715058ba01737a0a3903d02f7884b66388a9eae
Arun kumar <arunkumarjr1999@gmail.com>
2023-01-02 20:20:56 +0530

    Here Components are addes

---
+++ jakstudio-portfolio/src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <ul>
+        <Link to='/'><li>Home</li></Link>
+    </ul>
+  )
+}
+
+export default Navbar
